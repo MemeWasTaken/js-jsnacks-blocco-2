ef37728c2c9bc9f3fd18422705064ffaa403aaae
MemeWasTaken <memelazzari02@gmail.com>
2021-11-30 11:11:05 +0100

    Somma Numeri pos-Dispari: v.1

--- jsnack 2/js/script.js
+++ jsnack 2/js/script.js
@@ -1,2 +1,16 @@
 // jsnack 2
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+let numsList = [1, 2, 3, 4, 5, 8, 76, 43, 23, 21, 12,];
+let sum = 0;
+
+// console.log(numsList);
+
+for (let i = 0; i < numsList.length; i++) {
+
+    if ((i % 2) == 1) {
+        sum += numsList[i];
+    }
+}
+
+console.log(sum);
